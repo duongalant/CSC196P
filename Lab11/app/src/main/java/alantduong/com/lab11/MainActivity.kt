77e0e02ce7643e2f2cc7877b755bc44b8ae8f2e4
Alan Duong <alantduong@csus.edu>
2024-03-29 08:13:36 -0700

    Uploading Lab11 changes

--- Lab11/app/src/main/java/alantduong/com/lab11/MainActivity.kt
+++ Lab11/app/src/main/java/alantduong/com/lab11/MainActivity.kt
@@ -1,20 +1,73 @@
 package alantduong.com.lab11
 
-import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import android.os.Bundle
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    var currentLocation : Location? = null
+    var fusedLocationProviderClient : FusedLocationProviderClient? = null
+    private val REQUEST_CODE = 101
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        // Initialize fused location
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+
+        appUserMap()
+
+        }
+
+    private fun appUserMap() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+            != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+            != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this,arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), REQUEST_CODE)
+                return
+        }
+        // initialize task location
+        val task = fusedLocationProviderClient!!.lastLocation
+        task.addOnSuccessListener { location ->
+            if (location != null) {
+                currentLocation = location
+                val supportMapFragment = (supportFragmentManager.findFragmentById(R.id.myMap) as SupportMapFragment?)
+                    supportMapFragment!!.getMapAsync(this)
+            }
+        }
+    }
+
+    override fun onMapReady(mMap : GoogleMap) {
+        val myUserLocation = LatLng(currentLocation!!.latitude, currentLocation!!.longitude)
+        val markerOptions = MarkerOptions().position(myUserLocation).title("Your Location")
+
+        //zoom map
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(myUserLocation, 15f))
+
+        // add the maker on the map
+        mMap.addMarker(markerOptions)
+    }
+    override fun onRequestPermissionsResult(requestCode : Int, permissions : Array<out String>, grantResults : IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            REQUEST_CODE -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    appUserMap()
+                }
+            }
         }
     }
 }
